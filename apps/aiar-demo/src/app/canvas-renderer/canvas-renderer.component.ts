273f2bea091c9af31f4a2efef7a86e469f412386
David Moore <48303+vandermore@users.noreply.github.com>
2022-06-23 18:03:03 -0400

    Updated screen orientation and sizing to react to rotation (#1)
    Updated detection box styles, dropshadow, and color
    Removed scss
    Minor cleanup in unused tests
    
    Co-authored-by: David Moore <david.moore@oasisdigital.com>

--- apps/aiar-demo/src/app/canvas-renderer/canvas-renderer.component.ts
+++ apps/aiar-demo/src/app/canvas-renderer/canvas-renderer.component.ts
@@ -6,15 +6,20 @@ import {
   Output,
   EventEmitter,
   Input,
+  OnDestroy,
 } from '@angular/core';
 import { ImageAnnotation } from '../app.types';
+
+const RENDER_TIMING = 300;
+const DETECTION_BOX_COLOR = '#62b8c9';
+const DETECTION_TEXT_COLOR = '#62b8c9';
+const SHADOW_COLOR = 'rgb(50,50,50)';
 
 @Component({
   selector: 'aiar-canvas-renderer',
   templateUrl: './canvas-renderer.component.html',
-  styleUrls: ['./canvas-renderer.component.scss'],
 })
-export class CanvasRendererComponent implements AfterViewInit {
+export class CanvasRendererComponent implements AfterViewInit, OnDestroy {
   @Output()
   imageFrame = new EventEmitter<ImageData>();
 
@@ -29,16 +34,23 @@ export class CanvasRendererComponent implements AfterViewInit {
   private video = document.createElement('video');
   private detectionList: ImageAnnotation[] = [];
   private processing = false;
+  private animationFrame: number | undefined;
+
+  windowWidth: number | undefined = 1;
+  windowHeight: number | undefined = 1;
 
   constructor() {
     navigator.mediaDevices
       .getUserMedia({
         video: {
-          width: { min: 1280, max: 1280 },
-          height: { min: 720, max: 720 },
+          width: { ideal: 1920, max: 1920 }, // min: 1280, max: 1280 },
+          height: { ideal: 1080, max: 1080 }, // min: 720, max: 720 },
         },
+        audio: false,
       })
       .then((stream) => {
+        this.setVideoWindowUpdate(stream);
+
         this.video.srcObject = stream;
         this.video.play();
       });
@@ -48,40 +60,94 @@ export class CanvasRendererComponent implements AfterViewInit {
     this.render();
   }
 
+  ngOnDestroy() {
+    if (typeof this.animationFrame === 'number') {
+      cancelAnimationFrame(this.animationFrame);
+    }
+
+    //TODO:: Need to remove the screen.orientation event listener here
+  }
+
   render() {
-    requestAnimationFrame(() => {
+    this.animationFrame = requestAnimationFrame(() => {
       const canvas = this.canvasRef?.nativeElement;
       const ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.beginPath();
       ctx.drawImage(this.video, 0, 0, canvas.width, canvas.height);
-      const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
       if (!this.processing) {
         this.processing = true;
+        const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         this.imageFrame.emit(imgData);
-        setTimeout(() => (this.processing = false), 500);
+        setTimeout(() => (this.processing = false), RENDER_TIMING);
       }
+
       this.drawDetections(ctx);
       this.render();
     });
   }
 
-  drawImage(ctx: CanvasRenderingContext2D, imgData: ImageData) {
-    ctx.putImageData(imgData, 0, 0);
-  }
-
   drawDetections(ctx: CanvasRenderingContext2D) {
     this.detectionList.forEach((detection) => {
-      ctx.font = '15px Arial';
-      ctx.rect(
-        detection.box.left,
-        detection.box.top,
-        detection.box.width,
-        detection.box.height
-      );
-      ctx.stroke();
-      ctx.fillStyle = '#435a6b';
-      ctx.fillText(detection.label, detection.box.left, detection.box.top + 10);
+      this.drawSingleDetection(ctx, detection);
+    });
+  }
+
+  drawSingleDetection(
+    ctx: CanvasRenderingContext2D,
+    detection: ImageAnnotation
+  ) {
+    ctx.font = '15px Arial';
+    ctx.shadowOffsetX = 2;
+    ctx.shadowOffsetY = 2;
+    ctx.shadowColor = SHADOW_COLOR;
+    ctx.shadowBlur = 4;
+
+    ctx.strokeStyle = DETECTION_BOX_COLOR;
+    ctx.lineWidth = 2;
+    ctx.lineJoin = 'round';
+    ctx.rect(
+      detection.box.left,
+      detection.box.top,
+      detection.box.width,
+      detection.box.height
+    );
+    ctx.stroke();
+    ctx.fillStyle = DETECTION_TEXT_COLOR;
+    ctx.fillText(
+      detection.label,
+      detection.box.left + 5,
+      detection.box.top + 15
+    );
+  }
+
+  setVideoWindowSize(stream: MediaStream) {
+    const stream_settings = stream.getVideoTracks()[0].getSettings();
+
+    // actual width & height of the camera video
+    const stream_width = stream_settings.width;
+    const stream_height = stream_settings.height;
+
+    if (stream_width) {
+      this.windowWidth =
+        stream_width <= screen.availWidth ? stream_width : screen.availWidth;
+    }
+    if (stream_height) {
+      this.windowHeight =
+        stream_height <= screen.availHeight
+          ? stream_height
+          : screen.availHeight;
+    }
+  }
+
+  setVideoWindowUpdate(stream: MediaStream) {
+    //Only sets this size the on init. Does not account for screen rotation.
+    this.setVideoWindowSize(stream);
+
+    //Listens for change to account for screen rotation.
+    screen.orientation.addEventListener('change', () => {
+      this.setVideoWindowSize(stream);
     });
   }
 }
